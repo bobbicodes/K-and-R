a56d1e53eda5cdea6626613c9da69bfc8d39322d
Bobby Towers <sdfwer124@gmail.com>
2018-03-20 23:25:08 -0700

    Add files via upload

---
+++ input.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+void main()
+{
+    int x;
+    float f;
+    char c;
+    printf("Enter the int,float,char values :");
+    scanf("%d %f %c",&x,&f,&c);
+    printf("\nX=%d \nF=%f \nC=%c",x,f,c);
+}
+ 
+ 
